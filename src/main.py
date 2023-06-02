4f8e18f4c7ccf11b85b3a20533496a9ab549824e
fvalgreen <valverde130495@gmail.com>
2023-06-02 20:13:29 -0300

    core: migrations

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from src.core.middlewares.database_session_middleware import DatabaseSessionMiddleware
 from src.core.router import router
 
 
 def build_api() -> FastAPI:
 
   application = FastAPI()
+
+  application.add_middleware(DatabaseSessionMiddleware)
 
   application.include_router(router, prefix='/api')
 
