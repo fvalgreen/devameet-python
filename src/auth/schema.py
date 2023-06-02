2231869b2c29fe6b78d93fd8bc17e87a31cc68eb
fvalgreen <valverde130495@gmail.com>
2023-06-02 20:45:11 -0300

    feat(user): register schema

--- src/auth/schema.py
+++ src/auth/schema.py
@@ -1,6 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 
 class Login(BaseModel):
   login: str
   password: str
+
+
+class Register(BaseModel):
+  email: EmailStr
+  password: str = Field(..., min_length=8, max_length=20, regex='((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$')
+  name: str = Field(..., min_length=2)
+  avatar: str
