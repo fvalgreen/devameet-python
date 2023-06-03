8fec48f56e980bbd39bd53082e8aaf3b963e879c
fvalgreen <valverde130495@gmail.com>
2023-06-03 10:58:16 -0300

    feat(auth): jwt envs

--- src/core/config.py
+++ src/core/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
 
   log_level: str = 'info'
   db_url: str
+  jwt_secret: str
+  jwt_algorithm: str = 'HS256'
+  jwt_expiration: int = 3600
 
   class Config:
     env_file = '.env'
